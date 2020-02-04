ef92da954848e796aba1e52bbd9c31a031d7c8b7
plamingoh9009 <plamingoh9009@github.com>
2020-02-04 12:31:47 +0900

    2020.02.04 던그리드 베이스 업로드
    필요 없는 클래스를 지웠음
    던그리드 프로젝트의 베이스

---
+++ DungreedProject/mainGame.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"gameNode.h"
+
+class mainGame : public gameNode
+{
+public:
+	mainGame();
+	~mainGame();
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render(/*HDC hdc*/);
+};
+
--- 캐릭터정보창/forGameNode.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "imageManager.h"
-#define IMAGEMANAGER		imageManager::getSingleton()
-#define WINSIZEX	1024
-#define	WINSIZEY	768
